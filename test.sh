dd93bf161eb01a38bcb531c8af38a51c66d4f136
Lucy Davinhart <lucy@lucymhdavies.com>
2021-06-13 00:45:47 +0100

    test.sh: If an API call fails, output its body

--- test.sh
+++ test.sh
@@ -3,6 +3,18 @@
 set -e
 set -o pipefail
 
+# https://superuser.com/a/1641410
+# fail and output body
+curlf() {
+  OUTPUT_FILE=$(mktemp)
+  HTTP_CODE=$(curl --silent --output $OUTPUT_FILE --write-out "%{http_code}" "$@")
+  if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
+    >&2 cat $OUTPUT_FILE
+    return 22
+  fi
+  cat $OUTPUT_FILE
+  rm $OUTPUT_FILE
+}
 
 if [[ -z ${API_PATH} ]]; then
 	# Default to Test if unset
@@ -15,9 +27,9 @@ echo Testing Hello World
 echo ========================================
 echo
 
-curl --fail -s ${API_PATH}/hello | grep "HCL version"
-curl --fail -s ${API_PATH}/hello.hcl | grep "HCL version"
-curl --fail -s ${API_PATH}/hello.json | grep "JSON version"
+curlf -s ${API_PATH}/hello | grep "HCL version"
+curlf -s ${API_PATH}/hello.hcl | grep "HCL version"
+curlf -s ${API_PATH}/hello.json | grep "JSON version"
 
 
 echo
@@ -27,10 +39,10 @@ echo ========================================
 echo
 
 # Check we can access it
-curl --fail ${API_PATH}/v1/name
+curlf ${API_PATH}/v1/name
 
 # Check it shows name version
-if [[ $(curl --fail ${API_PATH}/v1/name.json | jq -r .version) == "1.2.0" ]]; then
+if [[ $(curlf ${API_PATH}/v1/name.json | jq -r .version) == "1.2.0" ]]; then
 	echo Name Contains Expected Version
 else
 	echo Unexpected name version $(curl ${API_PATH}/v1/name.json | jq .version)
@@ -44,7 +56,7 @@ echo ========================================
 echo
 
 # Check we can access it
-curl --fail ${API_PATH}/v1/front
+curlf ${API_PATH}/v1/front
 
 
 
@@ -62,8 +74,7 @@ if [[ "${API_PATH}" == "https://api.lmhd.me" ]]; then
 else
 	./issue-cert.sh
 fi
-#curl --fail -H "X-LMHD-Client-Cert: $(base64 cert.pem)" ${API_PATH}/v1/auth
-curl -H "X-LMHD-Client-Cert: $(base64 cert.pem)" ${API_PATH}/v1/auth
+curlf -H "X-LMHD-Client-Cert: $(base64 cert.pem)" ${API_PATH}/v1/auth
 
 
 echo
@@ -78,6 +89,5 @@ if [[ "${API_PATH}" == "https://api.lmhd.me" ]]; then
 else
 	./issue-cert.sh
 fi
-#curl --fail -H "X-LMHD-Client-Cert: $(base64 cert.pem)" -X POST ${API_PATH}/v1/front/federate
-curl -H "X-LMHD-Client-Cert: $(base64 cert.pem)" -X POST ${API_PATH}/v1/front/federate
+curlf -H "X-LMHD-Client-Cert: $(base64 cert.pem)" -X POST ${API_PATH}/v1/front/federate
 
